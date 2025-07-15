ccaa54e23433620f98bd0ff05888bf614779b42c
aloky0 <80944564+aloky0@users.noreply.github.com>
2025-07-15 11:56:22 +0300

    vless.sh

--- Vless.sh
+++ Vless.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-iptables -t mangle -D POSTROUTING -o br-lan -j TTL --ttl-set 65 2>/dev/null
-iptables -t mangle -A POSTROUTING -o br-lan -j TTL --ttl-set 65
+iptables -t mangle -D POSTROUTING -j TTL --ttl-set 65 2>/dev/null
 
+iptables -t mangle -A POSTROUTING -j TTL --ttl-set 65
 
-if ! grep -q "TTL --ttl-set 65" /etc/rc.local; then
-  sed -i '/exit 0/i iptables -t mangle -A POSTROUTING -o br-lan -j TTL --ttl-set 65' /etc/rc.local
-fi
+[ ! -f /etc/rc.local ] && echo -e "#!/bin/sh\nexit 0" > /etc/rc.local
+chmod +x /etc/rc.local
 
+grep -q 'TTL --ttl-set 65' /etc/rc.local || sed -i '/exit 0/i iptables -t mangle -A POSTROUTING -j TTL --ttl-set 65' /etc/rc.local
 
 rm -- "$0"
