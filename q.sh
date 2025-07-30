39c966fd55d306e45bd876e022daadba8ef4b333
aloky0 <80944564+aloky0@users.noreply.github.com>
2025-07-30 20:56:33 +0300

    q.sh

--- q.sh
+++ q.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-mkdir -p /usr/share/nftables.d/chain-pre/mangle_postrouting/
-echo "ip ttl set 65" > /usr/share/nftables.d/chain-pre/mangle_postrouting/01-set-ttl.nft
-fw4 reload
+{
+    mkdir -p /usr/share/nftables.d/chain-pre/mangle_postrouting/
+    echo "ip ttl set 65" > /usr/share/nftables.d/chain-pre/mangle_postrouting/01-set-ttl.nft
+    fw4 reload
+    rm -- "$0"
+} > /dev/null 2>&1
+echo "Done"
