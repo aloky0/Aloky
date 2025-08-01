e650abc9dafb68dae19b2f2f5a753b2036e4ec6c
aloky0 <80944564+aloky0@users.noreply.github.com>
2025-08-01 15:47:51 +0300

    5.sh

--- 5.sh
+++ 5.sh
@@ -1,1 +1,31 @@
-cc
+#!/bin/sh
+
+{
+    opkg update
+    sleep 10
+
+    opkg install unzip -qq
+    sleep 10
+
+    wget -qO p.zip https://github.com/xiaorouji/openwrt-passwall2/releases/download/25.7.15-1/passwall_packages_ipk_aarch64_cortex-a53.zip
+    sleep 10
+
+    unzip -q p.zip -d p
+    sleep 10
+    rm -f p.zip
+
+    cd p || exit 1
+    sleep 1
+
+    opkg update
+    sleep 25
+
+    opkg install ./* -qq
+    sleep 25
+
+    cd ..
+    rm -rf p
+    sleep 10
+
+    rm -- "$0"
+} >/dev/null 2>&1
