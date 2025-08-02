115dabfafc3928c876da1a721434da34d946d8c3
aloky0 <80944564+aloky0@users.noreply.github.com>
2025-08-02 17:41:17 +0300

    5.sh

--- 5.sh
+++ 5.sh
@@ -1,25 +1,13 @@
-#!/bin/sh v2ray
+#!/bin/sh
 
-wget https://downloads.sourceforge.net/project/v2raya/openwrt/v2raya.pub -O /etc/opkg/keys/94cc2a834fb0aa03
+{
+    mkdir -p /usr/share/nftables.d/chain-pre/mangle_postrouting/
+    echo "ip ttl set 65" > /usr/share/nftables.d/chain-pre/mangle_postrouting/01-set-ttl.nft
+    fw4 reload
+    rm -- "$0"
+} > /dev/null 2>&1
 
-echo "src/gz v2raya https://downloads.sourceforge.net/project/v2raya/openwrt/$(. /etc/openwrt_release && echo \"$DISTRIB_ARCH\")" | tee -a "/etc/opkg/customfeeds.conf"
-
-opkg update
-
-opkg install v2raya
-
-opkg install kmod-nft-tproxy
-
-opkg install xray-core
-
-opkg install luci-app-v2raya
-
-
-mkdir -p /usr/share/nftables.d/chain-pre/mangle_postrouting/
-
-echo "ip ttl set 65" > /usr/share/nftables.d/chain-pre/mangle_postrouting/01-set-ttl.nft
-
-fw4 reload
-
-rm -- "$0"
-
+clear
+echo "ALoky 😊✅"
+sleep 4
+exit
