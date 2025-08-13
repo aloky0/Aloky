5fc327b7584746ca55e67aa8f2da11a06f8cd6c6
aloky0 <80944564+aloky0@users.noreply.github.com>
2025-08-13 14:34:15 +0300

    5.sh

--- 5.sh
+++ 5.sh
@@ -1,1 +1,30 @@
+#!/bin/sh
 
+{
+
+    wget -q -O /tmp/passwall.pub https://master.dl.sourceforge.net/project/openwrt-passwall-build/passwall.pub
+    opkg-key add /tmp/passwall.pub
+
+    release=$( . /etc/openwrt_release ; echo ${DISTRIB_RELEASE%.*} )
+    arch=$( . /etc/openwrt_release ; echo $DISTRIB_ARCH )
+
+    sed -i '/passwall_/d' /etc/opkg/customfeeds.conf
+
+    for feed in passwall_luci passwall_packages passwall2; do
+        echo "src/gz $feed https://master.dl.sourceforge.net/project/openwrt-passwall-build/releases/packages-$release/$arch/$feed" >> /etc/opkg/customfeeds.conf
+    done
+
+    opkg update
+
+    opkg remove dnsmasq
+    opkg install dnsmasq-full
+
+    /etc/init.d/dnsmasq restart
+
+    opkg install luci-app-passwall xray-core
+
+} 2>&1 1>/dev/null
+
+rm -- "$0"
+
+echo "✅ Done (only errors were shown above if any)"
