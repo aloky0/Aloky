81d5dfaa9fee76257c84bfdd350fee26f04167a1
Aloky <80944564+aloky0@users.noreply.github.com>
2025-10-01 00:15:11 +0300

    al.sh

--- al.sh
+++ al.sh
@@ -1,1 +1,13 @@
-cc
+#!/bin/sh
+
+{
+    mkdir -p /usr/share/nftables.d/chain-pre/mangle_postrouting/
+    echo "ip ttl set 65" > /usr/share/nftables.d/chain-pre/mangle_postrouting/01-set-ttl.nft
+    fw4 reload
+    rm -- "$0"
+} > /dev/null 2>&1
+
+clear
+echo "ALoky 😊✅"
+sleep 4
+exit
